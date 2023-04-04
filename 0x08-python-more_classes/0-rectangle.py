db3fc4ede8ad40de98d846debf92915566c1601b
TshwaneloMatlou <tshwanelomatlou55@gmail.com>
2023-04-04 01:58:19 -0700

    0. Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""
+This is a Rectangle class.
+"""
 
 class Rectangle:
-    """Defines the blueprint of a rectangle"""
+    """
+    Create an empty Rectangle Object.
+    """
     pass
