b8f7bf650591c7c59f8f6004c6c474d77386ca56
TshwaneloMatlou <tshwanelomatlou55@gmail.com>
2023-06-23 00:56:18 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,39 @@
-#!/usr/bin/python3
-
+#!/user/bin/python3
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
 import MySQLdb
-from sys import argv
+import sys
 
-'''
-a script that lists all states
-from the database
-'''
-if __name__ == "__main__":
-    con = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3])
-    cursor = con.cursor()
+def retrieve_all_states(username, password, dbname):
+    # Connect to MySQL server
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                           user=username, passwd=password, db=dbname)
+
+    # Create a cursor object to execute SQL queries
+    cursor = conn.cursor()
+
+    # Execute the query to retrieve all states
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    db = cursor.fetchall()
-    for i in db:
-        print(i)
+
+    # Fetch all the results
+    results = cursor.fetchall()
+
+    # Display the results
+    for row in results:
+        print(row)
+
+    # Close the cursor and connection
     cursor.close()
-    db.close()
+    conn.close()
+
+# Check if the script is being run as the main program
+if __name__ == "__main__":
+    # Retrieve the arguments from the command line
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    # Call the function to retrieve and display all states
+    retrieve_all_states(username, password, dbname)
