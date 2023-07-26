75b4a68dfe6dcfc8ca9f2b50461fcb4a6cf64b9c
TshwaneloMatlou <tshwanelomatlou55@gmail.com>
2023-07-26 00:37:53 -0700

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,28 @@
 #!/usr/bin/node
+
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
   }
+  const todos = JSON.parse(body);
+  const uniqueUserIds = new Set();
+  todos.forEach(entry => {
+    uniqueUserIds.add(entry.userId);
+  });
+
+  const completed = {};
+  for (const i of uniqueUserIds) {
+    let n = 0;
+    for (const dos of todos) {
+      if (dos.userId === i && dos.completed) {
+        n++;
+      }
+    }
+    completed[i] = n;
+  }
+  console.log(completed);
 });
